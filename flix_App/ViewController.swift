841b37b8f755727b650bb98a059452033e252165
Dietrice Fortune <dietricefortune@dietrices-macbook-pro.local>
2018-10-19 21:39:53 -0400

    Initial Commit

---
+++ flix_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix_App
+//
+//  Created by Dietrice Fortune on 10/19/18.
+//  Copyright © 2018 Dietrice Fortune. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
